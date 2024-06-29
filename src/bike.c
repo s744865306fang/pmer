92cd604b28133b19db8d6554e5db5b818148dcb9
s744865306fang <2940039161@qq.com>
2024-06-29 19:51:08 +0800

    more pm to choose

--- src/bike.c
+++ src/bike.c
@@ -890,8 +890,8 @@ static u8 GetBikeCollisionAt(struct ObjectEvent *objectEvent, s16 x, s16 y, u8 d
 
 bool8 RS_IsRunningDisallowed(u8 tile)
 {
-    if (IsRunningDisallowedByMetatile(tile) != FALSE || gMapHeader.mapType == MAP_TYPE_INDOOR)
-        return TRUE;
+    if (IsRunningDisallowedByMetatile(tile) == TRUE)
+         return TRUE;
     else
         return FALSE;
 }
